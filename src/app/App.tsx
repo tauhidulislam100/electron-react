2b0994ef4072d1441eb5e8b2806118bbdf03f335
motailab <mdmotailab@gmail.com>
2024-04-15 13:50:01 +0600

    fixed depedency issue.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="container">
       <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-red-500">Hello world</h1>
+        <h1 className="text-red-500">Hello world it&apos;s working with vite and react</h1>
       </div>
     </div>
   );
