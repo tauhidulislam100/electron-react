8126e89188706c1547b60776d9d94a55b1d85f43
motailab <mdmotailab@gmail.com>
2024-04-15 13:50:00 +0600

    feat: set initial window size to 1024

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ async function createWindow() {
   }
 
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, "preload.js")
     }
